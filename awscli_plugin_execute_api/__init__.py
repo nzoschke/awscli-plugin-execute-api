94b558a63a22492013a8cc5556b881cdeef55466
Noah Zoschke <noah@mixable.net>
2018-12-22 09:40:52 -0800

    cleanup and comments

--- awscli_plugin_execute_api/__init__.py
+++ awscli_plugin_execute_api/__init__.py
@@ -4,7 +4,9 @@ from botocore.auth import SigV4Auth
 
 logger = logging.getLogger("awscli.clidriver")
 
-def auth_instance(self, signing_name, region_name, signature_version=None, **kwargs):
+def get_auth_instance(self, signing_name, region_name, signature_version=None, **kwargs):
+    # create new signer for API Gateway
+    # addresses `Credential should be scoped to correct service: 'execute-api'` error
     return SigV4Auth(self._credentials, "execute-api", region_name)
 
 def building_argument_table(argument_table, operation_model, event_name, session, **kwargs):
@@ -22,21 +24,20 @@ def building_argument_table(argument_table, operation_model, event_name, session
         logger.debug("Plugin awscli_plugin_execute_api: Config [%s] %s.%s not found", session.profile, svc, op)
         return
 
-    # build a hook that modifies the the request URL, headers and signature to work with
-    # API Gateway AWS_IAM authorized endpooint
+    # build and register a before-sign hook that modifies the request URL and monkeypatches the request signer
     def before_sign(request, operation_name, **kwargs):
         request.url = url
 
-        # Patch auth. Addresses `Credential should be scoped to correct service: 'execute-api'` error
-        kwargs['request_signer'].get_auth_instance = types.MethodType(auth_instance, kwargs['request_signer'])
-
         # Rename transport header. Addresses:
-        # The service name : "BackplaneExecutionService" identified from the payload does not agree with
-        # the service name : "DynamoDB_20120810" specified in the transport header
+        # The service name : "BackplaneExecutionService" identified from the payload does not agree with the service name : "DynamoDB_20120810" specified in the transport header
         if "X-Amz-Target" in request.headers:
             request.headers["X-Target"] = request.headers["X-Amz-Target"]
             del request.headers["X-Amz-Target"]
             logger.debug("Plugin awscli_plugin_execute_api: renamed X-Amz-Target %s", request.headers["X-Target"])
+
+        # patch signer and auth
+        signer = kwargs['request_signer']
+        signer.get_auth_instance = types.MethodType(get_auth_instance, signer)
 
     session.register("before-sign.%s.%s" % (svc, opName), before_sign)
 
